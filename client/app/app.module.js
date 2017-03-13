f2727f5d28ce93596fa496855ee15733866159aa
Brett Celestre <bcelestre@gmail.com>
2017-03-12 18:05:31 -0700

    Injects home.service to app.module

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,5 +1,6 @@
 let routes = require('./app.routes.js'),
     homeController = require('./components/home/homeController.js'),
+    homeService = require('./components/home/homeService.js'),
     converterController = require('./shared/converter/converterController.js'),
     converterService = require('./shared/converter/converterService.js')
 
@@ -7,6 +8,7 @@ angular.module('ExchangeRateConverter', [
   'ui.router',
   'appRoutes',
   'ExchangeRateConverter.home',
+  'ExchangeRateConverter.home.service',
   'ExchangeRateConverter.converter',
   'ExchangeRateConverter.converter.service'
 ]);
