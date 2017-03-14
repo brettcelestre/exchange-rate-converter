6b23acf28398ad03b57ce0ea647e5552d4b41d26
Brett Celestre <bcelestre@gmail.com>
2017-03-13 23:48:53 -0700

    Sets up API request

--- server/rates/ratesController.js
+++ server/rates/ratesController.js
@@ -1,7 +1,33 @@
 
+let appid = require('../config/config'),
+    request = require('request')
 
 module.exports = {
   findRates: function(req, res) {
+
+    console.log('appid = ', appid['appid']);
+
     console.log('ratesController findRates() - req.body: ', req.body);
+
+    let url = `https://openexchangerates.org/api/convert/${req.body.fromAmount}/${req.body.fromType}/${req.body.toType}?app_id=${req.body.appid}`
+
+    console.log(url);
+
+    request(url, function(error, response, body){
+      // Checks for errors
+      if (error) {
+        console.error('API Error = ', error);
+        return false;
+      }
+      // Parses body into JSON
+      var data = JSON.parse(body);
+      console.log('API DATA: ', data);
+
+      // Check data status before sending back
+      // TODO
+
+      // Sends response
+      res.status(200).send(data);
+    });
   }
 };
