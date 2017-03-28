3ce6bf72c090d2611e85cb152eaca6d738901d50
Brett Celestre <bcelestre@gmail.com>
2017-03-28 15:32:01 -0700

    Cleans up converter controller

--- client/app/shared/converter/converterController.js
+++ client/app/shared/converter/converterController.js
@@ -1,12 +1,7 @@
 
-angular.module('ExchangeRateConverter.converter', [])
+angular.module('ExchangeRateConverter.converter', ['ExchangeRateConverter.home.service', 'ExchangeRateConverter.converter.service'])
 
-.controller('ConverterController', function($scope, $rootScope, $state, ConverterService, HomeService, lodash) {
-
-  $scope.testLength = 4
-  $scope.testTrue = function() {
-    return true
-  }
+.controller('ConverterController', function($scope, $rootScope, ConverterService, HomeService, lodash) {
 
   $scope.fromCurrencyAmount = ConverterService.ConverterData.fromCurrencyAmount
   $scope.toCurrencyAmount = ConverterService.ConverterData.toCurrencyAmount
@@ -15,6 +10,9 @@ angular.module('ExchangeRateConverter.converter', [])
   $scope.currencies = ConverterService.ConverterData.currencies
   $scope.fromCurrencySelected = ConverterService.ConverterData.fromCurrencySelected
   $scope.toCurrencySelected = ConverterService.ConverterData.toCurrencySelected
+
+  $scope.invalid = false;
+  $scope.valid = false;
 
   // Sets from currency type
   $scope.setFromCurrencyType = function(symbol, fullTitle) {
@@ -58,27 +56,26 @@ angular.module('ExchangeRateConverter.converter', [])
         'toType': $scope.toType,
         'appid': HomeService.appid.id
       }
-      console.log('convertCurrency ran with ', data);
+      // console.log('convertCurrency ran with ', data);
 
       // Sends API req to /rates with data
       ConverterService.rates(data)
-      .then(function(data) {
-        console.log('ConverterService.rates data = THEN', data);
-
-        // If users App ID is incorrect
-        if ( data.data.status === 401 && data.data.message == 'invalid_app_id' ) {
-          console.log('invalid app id');
-          $rootScope.$emit('show-appid-invalid-error');
-        // If status is okay
-      } else if ( data.data.status === 200) {
-          console.log('ConverterService.rates data SUCCESS = ', data);
-          ConverterService.ConverterData.toCurrencyAmount = data.data.convertedAmount
-          $scope.toCurrencyAmount = ConverterService.ConverterData.toCurrencyAmount
-        }
-      })
-      .catch(function(data){
-        console.error('Error with login: ', data);
-      });
+        .then(function(data) {
+          // console.log('ConverterService.rates data = THEN', data);
+          // If users App ID is incorrect
+          if ( data.data.status === 401 && data.data.message == 'invalid_app_id' ) {
+            // console.log('invalid app id');
+            $rootScope.$emit('show-appid-invalid-error');
+          // If status is okay
+        } else if ( data.data.status === 200) {
+            // console.log('ConverterService.rates data SUCCESS = ', data);
+            ConverterService.ConverterData.toCurrencyAmount = data.data.convertedAmount
+            $scope.toCurrencyAmount = ConverterService.ConverterData.toCurrencyAmount
+          }
+        })
+        .catch(function(data){
+          console.error('Error with login: ', data);
+        });
     }
   }
 
